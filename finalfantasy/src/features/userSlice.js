dc21aeee1b911c3a4c7d3be3151682c5e1179d11
Larry Castillo <larrycastillo2009@gmail.com>
2021-10-13 04:55:52 -0500

    store integrate with index

--- finalfantasy/src/features/userSlice.js
+++ finalfantasy/src/features/userSlice.js
@@ -5,7 +5,7 @@ const userSlice = createSlice({
     initialState: {
         isSignedIn: false,
         userData: null,
-        searchInput: "tech"
+        searchInput: "tech",
         blogData: null,
 
     },
